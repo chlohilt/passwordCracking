68aed1cf51c5640e16d911f371d068e18f973005
chlohilt <hilton.chloe.y@gmail.com>
2024-01-24 14:49:06 -0700

    Simple dictionary attack password cracker with SHA256 hashes

--- simplePasswordCracking/Program.cs
+++ simplePasswordCracking/Program.cs
@@ -13,25 +13,29 @@ namespace passwordCracking
             String userPass = Console.ReadLine();
             byte[] userPassBytes = Encoding.UTF8.GetBytes(userPass);
 
+            Boolean found = false;
             using (SHA256 sha256Hash = SHA256.Create())
             {
                 byte[] hashBytes = sha256Hash.ComputeHash(userPassBytes);
-                string hashedData = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                string hashedUserPassData = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
 
-                Console.WriteLine("Hashed password using SHA-256: " + hashedData);
-            }
-            
-            String[] readText = File.ReadAllLines("../../../dictionary/2of12.txt");
-            Boolean found = false;
-            foreach (String s in readText)
-            {
-                if (userPass == s)
+                Console.WriteLine("Hashed password using SHA-256: " + hashedUserPassData);
+                
+                String[] readText = File.ReadAllLines("../../../dictionary/2of12.txt");
+                foreach (String s in readText)
                 {
-                    Console.WriteLine("Your password is too easy. Try again.");
-                    found = true;
+                    byte[] dictionaryItemBytes = Encoding.UTF8.GetBytes(s);
+                    byte[] hashDictBytes = sha256Hash.ComputeHash(dictionaryItemBytes);
+                    string hashedDictData = BitConverter.ToString(hashDictBytes).Replace("-", String.Empty);
+                    
+                    if (hashedUserPassData == hashedDictData)
+                    {
+                        Console.WriteLine("Your password is too easy. Try again.");
+                        found = true;
+                    }
                 }
             }
-
+            
             if (!found)
             {
                 Console.WriteLine("Your password was not found. Well done.");
