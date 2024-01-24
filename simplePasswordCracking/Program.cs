87abaec0dd454b9465162fa84eec60a0516bf367
chlohilt <hilton.chloe.y@gmail.com>
2024-01-24 14:30:43 -0700

    simple file IO pt 2

---
+++ simplePasswordCracking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace passwordCracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userPass = Console.ReadLine();
+            Console.WriteLine(userPass);
+            string[] readText = File.ReadAllLines("../../../dictionary/2of12.txt");
+            foreach (string s in readText)
+            {
+                if (userPass == s)
+                {
+                    Console.WriteLine("Your password is too easy. Try again.");
+                }
+            }
+        }
+    }
+}
