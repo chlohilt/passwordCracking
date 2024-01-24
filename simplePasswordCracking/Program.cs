2ff69bed30b0abec58c0ad289f0cd0e77087a2cf
chlohilt <hilton.chloe.y@gmail.com>
2024-01-24 14:41:10 -0700

    Adding SHA256 hashing

--- simplePasswordCracking/Program.cs
+++ simplePasswordCracking/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace passwordCracking
 {
@@ -7,15 +9,32 @@ namespace passwordCracking
     {
         static void Main(string[] args)
         {
-            string userPass = Console.ReadLine();
-            Console.WriteLine(userPass);
-            string[] readText = File.ReadAllLines("../../../dictionary/2of12.txt");
-            foreach (string s in readText)
+            Console.WriteLine("Please input your password in plain text (NEVER do this lol)");
+            String userPass = Console.ReadLine();
+            byte[] userPassBytes = Encoding.UTF8.GetBytes(userPass);
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] hashBytes = sha256Hash.ComputeHash(userPassBytes);
+                string hashedData = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+
+                Console.WriteLine("Hashed password using SHA-256: " + hashedData);
+            }
+            
+            String[] readText = File.ReadAllLines("../../../dictionary/2of12.txt");
+            Boolean found = false;
+            foreach (String s in readText)
             {
                 if (userPass == s)
                 {
                     Console.WriteLine("Your password is too easy. Try again.");
+                    found = true;
                 }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Your password was not found. Well done.");
             }
         }
     }
