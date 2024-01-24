77bd6cecf150e9b6ce8db1923d5a0b4419bcfcea
chlohilt <hilton.chloe.y@gmail.com>
2024-01-24 14:30:19 -0700

    Simple file IO

--- handout/InvalidSignatureException.cs
+++
@@ -1,9 +0,0 @@
-
-public class InvalidSignatureException : SystemException {
-
-    public InvalidSignatureException() { }
-
-    public InvalidSignatureException(string message) : base(message) { }
-
-    public InvalidSignatureException(string message, Exception inner) : base(message, inner) { }
-}
---
+++ simplePasswordCracking/README.md
@@ -0,0 +1,3 @@
+# Simple Password Cracking
+
+This is a very simple password cracker using a dictionary. This is more of an exercise to familiarize myself with C# for CS465 at BYU.
